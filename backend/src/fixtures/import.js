412c4dde6d1c0f3ad98547e66977936a843deebe
Petr Němeček <thenemecek@gmail.com>
2019-03-31 11:10:03 +0200

    Fixed importing fixtures async issues

--- backend/src/fixtures/import.js
+++ backend/src/fixtures/import.js
@@ -1,99 +1,100 @@
-import teams from './data/teams'
+/* eslint-disable no-console */
+import { Language, Preset, Role, Team } from '../db'
+
 import languages from './data/languages'
-import roles from './data/roles'
 import presets from './data/presets'
-import { Team, Language, Role, Preset } from '../db'
+import roles from './data/roles'
+import teams from './data/teams'
 
 const langDocs = {}
 const teamDocs = {}
 const roleDocs = {}
 
-async function importLanguages () {
+async function importLanguages() {
   await Language.deleteMany({})
 
-  await Promise.all(languages.map(async (language) => {
-    const langDoc = new Language(language)
-    langDocs[language.code] = langDoc
-    await langDoc.save(function (err) {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log(`Language ${language.name} saved`)
-      }
+  return Promise.all(
+    languages.map((language) => {
+      const langDoc = new Language(language)
+      langDocs[language.code] = langDoc
+      return langDoc
+        .save()
+        .then(() => console.log(`Language ${language.name} saved`))
+        .catch((err) => console.log(err))
     })
-  }))
+  )
 }
 
-async function importTeams () {
+async function importTeams() {
   await Team.deleteMany({})
 
-  await Promise.all(teams.map(async (team) => {
-    const teamDoc = new Team({
-      code: team.code,
-      translations: Object.keys(langDocs).map((lang) => ({
-        locale: langDocs[lang],
-        name: team.translations[lang].name,
-        description: team.translations[lang].description
-      }))
+  return Promise.all(
+    teams.map((team) => {
+      const teamDoc = new Team({
+        code: team.code,
+        translations: Object.keys(langDocs).map((lang) => ({
+          locale: langDocs[lang],
+          name: team.translations[lang].name,
+          description: team.translations[lang].description,
+        })),
+      })
+      teamDocs[team.code] = teamDoc
+      return teamDoc
+        .save()
+        .then(() => console.log(`Team ${team.code} saved`))
+        .catch((err) => console.log(`Team ${team.code} error: `, err))
     })
-    teamDocs[team.code] = teamDoc
-    await teamDoc.save(function (err) {
-      if (err) {
-        console.log(`Team ${team.code} error: `, err)
-      } else {
-        console.log(`Team ${team.code} saved`)
-      }
-    })
-  }))
+  )
 }
 
-async function importRoles () {
+async function importRoles() {
   await Role.deleteMany({})
 
-  await Promise.all(roles.map(async (role) => {
-    const roleDoc = new Role({
-      ...role,
-      team: teamDocs[role.team],
-      translations: Object.keys(langDocs).map((lang) => ({
-        locale: langDocs[lang],
-        name: role.translations[lang].name,
-        description: role.translations[lang].description
-      }))
+  return Promise.all(
+    roles.map((role) => {
+      const roleDoc = new Role({
+        ...role,
+        team: teamDocs[role.team],
+        translations: Object.keys(langDocs).map((lang) => ({
+          locale: langDocs[lang],
+          name: role.translations[lang].name,
+          description: role.translations[lang].description,
+        })),
+      })
+      roleDocs[role.code] = roleDoc
+      return roleDoc
+        .save()
+        .then(() => console.log(`Role ${role.code} saved`))
+        .catch((err) => console.log(err))
     })
-    roleDocs[role.code] = roleDoc
-    await roleDoc.save(function (err) {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log(`Role ${role.code} saved`)
-      }
-    })
-  }))
+  )
 }
 
-async function importPresets () {
+async function importPresets() {
   await Preset.deleteMany({})
 
-  await Promise.all(presets.map(async (preset) => {
-    const presetDoc = new Preset({
-      name: preset.name,
-      roles: preset.roles.map((role) => roleDocs[role])
+  return Promise.all(
+    presets.map((preset) => {
+      const presetDoc = new Preset({
+        name: preset.name,
+        roles: preset.roles.map((role) => roleDocs[role]),
+      })
+      return presetDoc
+        .save()
+        .then(() => console.log(`Preset ${preset.name} saved`))
+        .catch((err) => console.log(err))
     })
-    await presetDoc.save(function (err) {
-      if (err) {
-        console.log(err)
-      } else {
-        console.log(`Preset ${preset.name} saved`)
-      }
-    })
-  }))
+  )
 }
 
-async function importAll () {
+async function importAll() {
   await importLanguages()
   await importTeams()
   await importRoles()
-  // await importPresets()
+  await importPresets()
 }
 
-importAll()
+// eslint-disable-next-line
+;(async () => {
+  await importAll()
+})()
