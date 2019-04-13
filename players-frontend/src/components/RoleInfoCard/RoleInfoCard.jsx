9ab70978eee57a8aacb006bc9ea4cd6dbe257e41
Petr Němeček <thenemecek@gmail.com>
2019-04-13 19:52:42 +0200

    Fixed role info card component translations

--- players-frontend/src/components/RoleInfoCard/RoleInfoCard.jsx
+++ players-frontend/src/components/RoleInfoCard/RoleInfoCard.jsx
@@ -2,19 +2,26 @@ import React, { Fragment } from 'react'
 
 import PropTypes from 'prop-types'
 import Typography from '@material-ui/core/Typography'
+import { injectIntl } from 'react-intl'
 import styles from './styles'
 import { withStyles } from '@material-ui/core'
 
-const RoleInfoCard = ({ classes, role }) => (
+const RoleInfoCard = ({ intl, classes, role }) => (
   <Fragment>
-    <Typography className={classes.name}>{role.name}</Typography>
-    <Typography className={classes.description}>{role.description}</Typography>
+    <Typography className={classes.name}>
+      {intl.formatMessage({ id: role.name })}
+    </Typography>
+    <Typography className={classes.description}>
+      {intl.formatMessage({ id: role.description })}
+    </Typography>
   </Fragment>
 )
 
 RoleInfoCard.propTypes = {
+  intl: PropTypes.object.isRequired,
+
   classes: PropTypes.object.isRequired,
   role: PropTypes.object.isRequired,
 }
 
-export default withStyles(styles)(RoleInfoCard)
+export default injectIntl(withStyles(styles)(RoleInfoCard))
