1c7e20fa0fe82cefa219ca11f5d1a75397fcf5f9
Petr Němeček <thenemecek@gmail.com>
2019-08-06 21:09:43 +0200

    Fix webpack loading ECMAScript modules

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -25,6 +25,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.mjs$/,
+        type: 'javascript/auto',
+      }
     ],
   },
   resolve: {
