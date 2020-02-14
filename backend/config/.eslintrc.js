90568883af2b6d207ef9a5cc12fb8796fc856424
Petr Němeček <thenemecek@gmail.com>
2020-02-14 15:09:20 +0100

    Remove Babel; Install eslint for TS

--- backend/config/.eslintrc.js
+++ backend/config/.eslintrc.js
@@ -1,3 +1,27 @@
 module.exports = {
-  extends: ['standard', 'prettier', 'eslint:recommended'],
+  env: {
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['prettier', '@typescript-eslint'],
+  extends: [
+    'standard',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.d.ts', '.tsx', '.js', '.jsx'],
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  rules: {
+    'prettier/prettier': 'error',
+  },
 }
